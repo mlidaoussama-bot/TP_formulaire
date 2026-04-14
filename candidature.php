1ec63d63717998cee328c7fbf3e1282d4ec274a1
Mlida <mlidaoussama@gmail.com>
2026-04-14 14:35:01 +0200

    structure html et initialisation des variables

---
+++ candidature.php
@@ -0,0 +1,23 @@
+<?php
+$prenom = '';
+$nom = '';
+$email = '';
+$email_confirm = '';
+$age = '';
+$filiere = '';
+$motivation = '';
+$reglement = false;
+$erreurs = [];
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
